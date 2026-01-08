20c650738515881be6f030f474cfe774a1fd34cf
alexandraciorba06 <alexandra.jeniffer.ciorba@stud.ubbcluj.ro>
2026-01-08 21:32:26 +0200

    1

---
+++ MobileApplication1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileApplication1_
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MobileApplication1/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileApplication1_
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
